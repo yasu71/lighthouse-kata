99dfcf399da957e49989e06ff1d7d425fe19fd76
Yasu71 <yasuyo71@gmail.com>
2020-02-12 04:07:14 +0000

    Add more conditional examples

--- lighthouse-js-fundamentals/conditionals.js
+++ lighthouse-js-fundamentals/conditionals.js
@@ -20,3 +20,10 @@ if (isCitizen && age > 18) {
   console.log("You are eligible to vote.");
 }
 
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
